2544f65037f62ef5157ead66fc95e69c82f2151e
idhamyahya <idhamyahya72@gmail.com>
2026-08-24 14:49:06 +0700

    Membuat server

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("HALOOOO");
+});
+
+app.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
+});

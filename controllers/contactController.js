2a014183008c9d657183f67fc8aa0786e05f7f65
idhamyahya <idhamyahya72@gmail.com>
2026-08-25 10:36:31 +0700

    Menambahkan fitur cari contact by Id

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,7 +1,13 @@
-import { getContact } from "../models/contactModel.js";
+import { getContact, getContactById } from "../models/contactModel.js";
 
 export const getAllContact_C = (req, res) => {
   const contact = getContact();
+  res.json(contact);
+  return contact;
+};
+
+export const getContactById_C = (req, res) => {
+  const contact = getContactById(req.params.id);
   res.json(contact);
   return contact;
 };
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { getAllContact_C } from "../controllers/contactController.js";
+import {
+  getAllContact_C,
+  getContactById_C,
+} from "../controllers/contactController.js";
 
 const router = express.Router();
 
 router.get("/contacts", getAllContact_C);
+router.get("/contacts/:id", getContactById_C);
 
 export default router;
